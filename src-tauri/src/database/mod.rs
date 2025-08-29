10b0c3b0c7af3785659a6ce81919250dd73c82c4
karczuRF <oskar.karcz@rumblefish.pl>
2025-08-29 11:34:38 +0200

    refactor establish connection

--- src-tauri/src/database/mod.rs
+++ src-tauri/src/database/mod.rs
@@ -2,23 +2,16 @@ pub mod models;
 pub mod schema;
 pub mod store;
 
-use diesel::prelude::*;
-use diesel::sqlite::Sqlite;
-use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use log::info;
+use sqlx::{sqlite::SqlitePoolOptions, SqlitePool};
 
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./migrations");
 pub const LOG_TARGET: &str = "tari::universe::binary_manager";
 
-pub fn establish_connection(db_url: &str) -> SqliteConnection {
-    info!(target: LOG_TARGET,"Establishing db connection url: {:?}", db_url);
-    let mut db_connection = SqliteConnection::establish(db_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", db_url));
-    run_migrations(&mut db_connection).unwrap(); // TODO handle migrations error while running setup https://github.com/orgs/tari-project/projects/18/views/1?pane=issue&itemId=63753279
-    db_connection
-}
-fn run_migrations(connection: &mut impl MigrationHarness<Sqlite>) -> Result<(), ()> {
-    connection.run_pending_migrations(MIGRATIONS).unwrap();
-
-    Ok(())
+pub async fn establish_connection(db_url: &str) -> SqlitePool {
+    info!(target: LOG_TARGET, "Establishing db connection url: {:?}", db_url);
+    SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect(db_url)
+        .await
+        .expect("Failed to create SQLx pool")
 }
